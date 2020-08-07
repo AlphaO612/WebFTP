4fdec5e74343ac47712aaf0bd2f8e8869b8995f8
Alpha <41066028+AlphaO612@users.noreply.github.com>
2020-08-07 17:05:40 +0300

    Add files via upload

--- web.py
+++ web.py
@@ -13,23 +13,32 @@ dir = os.listdir(pwd)
 def index():
     return str(token)
 
+def readStorage(name):
+    f = open(pwd + name, 'r')
+    set = f.read()
+    f.close()
+    return set
+
 @app.route(f"/file/{token}", methods=['GET'])
 def files():
     if not request.args.get('path', '') == '':
         pwd = request.args.get('path', '')
-        dir = os.listdir(pwd)
-        text = ''
-        for i in dir:
-            if os.path.isfile(pwd + '/' + i):
-                text += f'<br><a href="/file/{token}?path={pwd}/{i}">{i}</a> - <a href="/file/{token}/{os.path.basename(pwd + "/" + i)}?path={pwd}" style="color: red;" download>(!)</a>'
-            else:
-                text += f'<br><a href="/file/{token}?path={pwd}/{i}">{i}</a>'
-        return f'<h3> files in {pwd}</h3>' + text
+        if os.path.isdir(pwd):
+            pwd = request.args.get('path', '')
+            dir = os.listdir(pwd)
+            text = ''
+            for i in dir:
+                if os.path.isfile(pwd + '/' + i):
+                    text += f'<br><a href="/file/{token}?path={pwd}/{i}">{i}</a> - <a href="/file/{token}/{os.path.basename(pwd + "/" + i)}?path={pwd}" style="color: red;" download>(!)</a>'
+                else:
+                    text += f'<br><a href="/file/{token}?path={pwd}/{i}">{i}</a>'
+            return f'<h3> files in {pwd}</h3>' + text
+        else:
+            return readStorage(pwd)
     else:
         pwd = os.getcwd()
         dir = os.listdir(pwd)
         return redirect(url_for('files', path=pwd))
-    
 
 @app.route(f"/file/{token}/<filename>", methods=['GET'])
 def dl(filename):
@@ -40,4 +49,4 @@ with app.test_request_context():
     print(f"--------------------------------------------------\nSite - {url_for('files', path=pwd)}\n--------------------------------------------------")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
